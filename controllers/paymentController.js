3f802855cbdab2a96a9c09b501c1cca7756131ea
Ritik Kumar Pathak <phritik06@gmail.com>
2024-03-21 23:01:29 +0530

    chnage

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -42,12 +42,9 @@ exports.paymentVerification = async (req, res) => {
       razorpay_signature,
     });
 
-    // res.redirect(
-    //   // `https://wonderstay.vercel.app/paymentsuccess?reference=${razorpay_payment_id}`
-      res.redirect(
-        `https://wonderstay.vercel.app`
+    res.redirect(
+      `https://wonderstay.vercel.app/paymentsuccess?reference=${razorpay_payment_id}`
       )
-    // );
   } else {
     res.status(400).json({
       success: false,
