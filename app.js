40ac525ecf666715e02a5e8bb72c67e1930681bb
Ritik Kumar Pathak <phritik06@gmail.com>
2024-03-21 14:23:40 +0530

    chnages

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ app.use(express.json({}));
 app.use(cookieParser());
 app.use(
   cors({
-    // origin:"http://localhost:5173",
-    origin: "https://wonderstay.vercel.app",
+    origin:"http://localhost:5173",
+    // origin: "https://wonderstay.vercel.app",
     credentials: true,
   })
 );
